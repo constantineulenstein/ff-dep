b0db7c0ed0715ddc1050e0442c98ff142cff2039
Constantin Eulenstein <constantineulenstein@DN51s4rr.SUNet>
2022-05-23 08:36:07 -0700

    added apostrophes for restauurant details js

--- client/components/z_SampleData/restaurant_details.js
+++ client/components/z_SampleData/restaurant_details.js
@@ -10,16 +10,16 @@ import r9 from "../../sample_data/restaurants/ChIJw4w5aNS6j4AR9LgOtniCDno/place_
 import r10 from "../../sample_data/restaurants/ChIJYRHjWf67j4ARClGtTLToHZk/place_details_clean.json";
 
 restaurant_details_data = {
-  ChIJg1ayR7iwj4ARBsM83DizgKA: r1,
-  ChIJs2q9C226j4ARohCdKz3cBH8: r2,
-  ChIJsUowmuW6j4ARncdA5qucqUk: r3,
+  "ChIJg1ayR7iwj4ARBsM83DizgKA": r1,
+  "ChIJs2q9C226j4ARohCdKz3cBH8": r2,
+  "ChIJsUowmuW6j4ARncdA5qucqUk": r3,
   "ChIJT-J4Xze7j4ARYwN9-k8LDts": r4,
-  ChIJtRLy85ywj4AR38KYK7uJ0Yc: r5,
-  ChIJTzWDpDO3j4ARI08nSK_WLeU: r6,
+  "ChIJtRLy85ywj4AR38KYK7uJ0Yc": r5,
+  "ChIJTzWDpDO3j4ARI08nSK_WLeU": r6,
   "ChIJu_26IQm6j4AR_Pl-EyDdOwU": r7,
-  ChIJu87f3CK7j4ARsO7lTOp9CIQ: r8,
-  ChIJw4w5aNS6j4AR9LgOtniCDno: r9,
-  ChIJYRHjWf67j4ARClGtTLToHZk: r10,
+  "ChIJu87f3CK7j4ARsO7lTOp9CIQ": r8,
+  "ChIJw4w5aNS6j4AR9LgOtniCDno": r9,
+  "ChIJYRHjWf67j4ARClGtTLToHZk": r10,
 };
 
 export default restaurant_details_data;
